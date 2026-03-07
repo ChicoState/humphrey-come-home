24659cc7b217b289eda5f213210a00b7a9eb7322
ImplodedBrownies <rubalbajwa05@gmail.com>
2026-03-06 16:27:30 -0800

    Edited DATABASE object to support postgresql

--- humphreyComeHome/settings.py
+++ humphreyComeHome/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'humphreyComeHome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
