ef3ad87bb192176019dd46082072124fe350f6cd
ImplodedBrownies <rubalbajwa05@gmail.com>
2026-03-08 19:36:03 -0700

    Fixed Shelters model to include geolocation

--- shelters/models.py
+++ shelters/models.py
@@ -11,6 +11,12 @@ class Shelter(models.Model):
     longitude = models.FloatField(blank = True, null = True)
     website = models.URLField(blank = True, null = True)
     phone = models.CharField(max_length = 50, blank = True, null = True)
+    location = models.PointField(srid = 4326, null = True, blank = True)
+    
+    def save(self, args, **kwargs):
+        if self.longitude and self.latitude:
+            self.location = Point(self.longitude, self.latitude, srid = 4326)
+        super().save(args, **kwargs)
 
     def __str__(self):
         return self.name
