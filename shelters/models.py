0725a5bb0e4f70cd07af37c867fc1a86f41e0d84
Breachico <blemery@csuchico.edu>
2026-03-08 23:36:33 -0700

    Fixed bug in shelter/models.py args should have been *args in save() function.

--- shelters/models.py
+++ shelters/models.py
@@ -14,10 +14,10 @@ class Shelter(models.Model):
     phone = models.CharField(max_length = 50, blank = True, null = True)
     location = models.PointField(srid = 4326, null = True, blank = True)
     
-    def save(self, args, **kwargs):
+    def save(self, *args, **kwargs):
         if self.longitude and self.latitude:
             self.location = Point(self.longitude, self.latitude, srid = 4326)
-        super().save(args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
